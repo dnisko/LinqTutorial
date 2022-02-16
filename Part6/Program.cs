385734fef85b50a631e18f8d1a97607ab6b73969
dnisko <dnisko@hotmail.com>
2022-02-16 23:41:50 +0100

    Part8... Part6 ima detali za onlinebooking

---
+++ Part6/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PrvDel();
+            //VtorDel();
+            Test();
+        }
+
+        public static void Test()
+        {
+            TestDBContext test = new TestDBContext();
+            IEnumerable<objekt> objekts = test.objekts;
+            Console.OutputEncoding = Encoding.UTF8;
+            foreach (objekt obj in objekts)
+            {
+                Console.WriteLine("Objekt = " + obj.ime);
+
+                IEnumerable<nastan> nastans = obj.nastans.Where(x => x.objekt.id_objekt == x.o_id_objekt);
+
+                foreach (nastan nas in nastans)
+                {
+                    Console.WriteLine("\tNastan = " + nas.naziv);
+                }
+                    //.Where(x => x.ime);
+            }
+
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+        public static void VtorDel()
+        {
+            EmployeeDBContext context = new EmployeeDBContext();
+            IEnumerable<Department> Departments = context.Departments
+                .Where(x => x.Name == "IT" || x.Name == "HR");
+
+            foreach (Department dept in Departments)
+            {
+                Console.WriteLine("Department - " + dept.Name);
+
+                IEnumerable<Employee> employees = dept.Employees
+                    .Where(x => x.Gender == "Male");
+
+                foreach (Employee employee in employees)
+                {
+                    Console.WriteLine("\tEmployee Name = " +
+                        employee.FirstName + " " +
+                        employee.LastName);
+                }
+            }
+
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+        private static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        public static void PrvDel()
+        {
+            List<int> Numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //Func<int, bool> predicate = x => x % 2 == 0;
+            //IEnumerable<int> evenNumbers = Numbers.Where(num => IsEven(num));
+
+            //IEnumerable<int> evenNumbers = from num in Numbers
+            //                               where num % 2 == 0
+            //                               select num;
+            var result = Numbers
+                .Select((num, index) =>
+                new
+                {
+                    Number = num,
+                    Index = index
+                })
+                .Where(x => x.Number % 2 == 0)
+                .Select(x => x.Index);
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            //Console.WriteLine(result);
+
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Part7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var result = Employee.GetAllEmployees()
+                .Select(emp => new
+                {
+                    Name = emp.FirstName,
+                    Salary = emp.AnnualSalary,
+                    Bouns = emp.AnnualSalary * 0.1
+                })
+                .Where(sal => sal.Salary > 50000);
+
+            foreach (var v in result)
+            {
+                Console.WriteLine(v.Name + ": " + v.Salary + " - " + v.Bouns);
+            }
+            Console.WriteLine("Press enter to close...");
+            Console.ReadLine();
+        }
+    }
+}
